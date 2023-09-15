eb8506a5a0721b7533d0817a37db30b196469524
aeon <jerry73204@gmail.com>
2023-09-15 10:10:56 +0800

    Automatically build binary in 3-flow test.

--- scripts/run_3_flow.sh
+++ scripts/run_3_flow.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+cargo build --release --bin server_pn
+
 parallel -j0 <<EOF
 ./target/release/server --listen-addr=192.168.1.1:55553 --priority=3
 ./target/release/server --listen-addr=192.168.1.1:55551 --priority=1
--- scripts/run_3_flow_pn.sh
+++ scripts/run_3_flow_pn.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+cargo build --release --bin server_pn
+
 parallel -j0 <<EOF
 ./target/release/server_pn --listen-addr=192.168.1.1:55553 --priority=3 >p1.out
 ./target/release/server_pn --listen-addr=192.168.1.1:55551 --priority=1 >p2.out
