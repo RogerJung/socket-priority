cd70d69ade1d7fea1d5eae53aa874d94c03e2e26
aeon <jerry73204@gmail.com>
2023-09-15 10:21:35 +0800

    Tweak set-taprio.sh to allow priority-0 traffic.

--- scripts/set-taprio.sh
+++ scripts/set-taprio.sh
@@ -12,9 +12,9 @@ sudo tc qdisc replace dev "$dev" parent root handle 100 taprio \
      map 0 1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 \
      queues 1@0 1@1 1@2 \
      base-time 200 \
-     sched-entry S 00 300000 \
-     sched-entry S 02 300000 \
-     sched-entry S 04 800000 \
+     sched-entry S 01 300000 \
+     sched-entry S 03 300000 \
+     sched-entry S 05 800000 \
      flags 0x1 \
      txtime-delay 200000 \
      clockid CLOCK_TAI
