2c1f4502426a4adb9b68b7eaead6ad0040698681
aeon <jerry73204@gmail.com>
2023-09-15 10:18:32 +0800

    Show usage in run_{client,server} scripts.

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -5,9 +5,13 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$script_dir/.."
 
 dir="$1"
-shift
+shift || {
+    echo "Usage: $0 <OUT_DIR>" >&2
+    exit 1
+}
 
 mkdir "$dir"
+cargo build --release --all-targets
 
 for prio in {0..6}; do
     echo "./target/release/server -l 192.168.1.1:5555${prio} -p ${prio} > $dir/p${prio}.std 2> $dir/p${prio}.err"
