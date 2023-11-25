f7e6b791ce826c593ece40c16d8fcb5e5bbd2bf8
aeon <jerry73204@gmail.com>
2023-11-25 23:37:33 +0800

    Run rustfmt

--- src/bin/client_cb.rs
+++ src/bin/client_cb.rs
@@ -3,13 +3,13 @@ use clap::Parser;
 use nix::sys::socket::sockopt::Priority;
 use nix::sys::socket::{getsockopt, setsockopt};
 //use std::io::Write;
+use net2::TcpBuilder;
+use socket2::{Domain, Socket, Type};
+use std::io::{Read, Write};
 use std::net::{SocketAddr, TcpStream};
 use std::os::unix::io::AsRawFd;
-use std::time::{Duration, Instant};
-use std::io::{Read, Write};
-use socket2::{Socket, Domain, Type};
-use net2::TcpBuilder;
 use std::process;
+use std::time::{Duration, Instant};
 
 #[derive(Debug, Clone, Parser)]
 struct Opts {
@@ -25,7 +25,7 @@ struct Opts {
     #[clap(short = 's', long)]
     pub payload_size: usize,
 
-    #[clap(short = 'T', long, default_value ="200")]
+    #[clap(short = 'T', long, default_value = "200")]
     pub period: String,
 }
 
@@ -41,7 +41,6 @@ fn main() -> Result<()> {
     let mut stream = bound_tcp.connect(&opts.connect_addr)?;
     // Bind to the specified local address and port
     //stream.bind(&opts.bind_addr.into())?;
-    
 
     // Set priority
     let fd = stream.as_raw_fd();
@@ -55,10 +54,9 @@ fn main() -> Result<()> {
     let mut since = Instant::now();
     let mut acc = 0;
     let period: u64 = opts.period.parse().unwrap_or(1) as u64;
-    let start=Instant::now();
+    let start = Instant::now();
     let mut round_trip_times: Vec<Duration> = Vec::new();
     loop {
-
         stream.set_nonblocking(true)?;
 
         let mut reply_buf = [0u8; 256];
@@ -68,7 +66,6 @@ fn main() -> Result<()> {
             if bytes_read == 0 {
                 //println!("No data read.");
             } else {
-                
                 let received_time_bytes = &reply_buf[..16];
                 //println!("received_time_bytes: {received_time_bytes:?}", received_time_bytes = received_time_bytes);
                 let duration_value = u128::from_le_bytes(received_time_bytes.try_into().unwrap());
@@ -78,33 +75,38 @@ fn main() -> Result<()> {
 
                 //println!("Send Time in Seconds: {:.6}", send_time_seconds); // 格式化為小數點後 6 位的浮點數
                 //println!("send_time: {send_time:?}", send_time = send_time);
-                let receive_time = Instant::now()-start;
+                let receive_time = Instant::now() - start;
                 //let round_trip_time=(receive_time-send_time).as_micros();
-                let round_trip_time=receive_time-send_time;
-                println!("Round trip time: {round_trip_time:?}", round_trip_time = round_trip_time);
+                let round_trip_time = receive_time - send_time;
+                println!(
+                    "Round trip time: {round_trip_time:?}",
+                    round_trip_time = round_trip_time
+                );
                 round_trip_times.push(round_trip_time);
             }
-        } 
-
+        }
 
         let elapsed = since.elapsed();
         if elapsed >= Duration::from_micros(period) {
-            let send_time=Instant::now() - start;
+            let send_time = Instant::now() - start;
             let send_time_bytes = send_time.as_micros().to_le_bytes();
             //println!("send_time_bytes: {send_time_bytes:?}", send_time_bytes = &send_time_bytes);
             let mut buf = vec![0u8; opts.payload_size];
-            buf.splice(..std::mem::size_of::<u128>(), send_time_bytes.iter().cloned());
-            
+            buf.splice(
+                ..std::mem::size_of::<u128>(),
+                send_time_bytes.iter().cloned(),
+            );
+
             //println!("buf: {buf:?}", buf = &buf);
             stream.write_all(&buf)?;
             since = Instant::now();
             acc = 0;
         }
-        
+
         // 在程式運行 3 秒後停止整個程式
         if start.elapsed() >= Duration::from_secs(1) {
             //println!("Program is stopping after 3 seconds.");
-            
+
             for rtt in &round_trip_times {
                 println!("round_trip_times: {:?}", rtt);
             }
@@ -113,7 +115,7 @@ fn main() -> Result<()> {
                 let avg_time = total_time / round_trip_times.len() as u32;
                 let min_time = round_trip_times.iter().min().unwrap();
                 let max_time = round_trip_times.iter().max().unwrap();
-        
+
                 println!("Average time: {:?}", avg_time);
                 println!("Minimum time: {:?}", min_time);
                 println!("Maximum time: {:?}", max_time);
