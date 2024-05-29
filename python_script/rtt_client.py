940284ac74e55c2258a37640682844bc16de7ea2
RogerJung <engineer54rj@gmail.com>
2024-05-29 13:18:28 +0800

    Update rtt client.

--- python_script/rtt_client.py
+++ python_script/rtt_client.py
@@ -4,6 +4,7 @@ import argparse
 import os
 
 HOST=os.getenv('SERVER_IP')
+DATA_SIZE=500000
 
 parser = argparse.ArgumentParser(description="Socket Client with Python.")
 
@@ -11,7 +12,10 @@ parser.add_argument("--port", "-p", type=int, required=True)
 parser.add_argument("--priority", "-i", type=int, required=True)
 args = parser.parse_args()
 
+
 def measure_rtt(message, timeout=2):
+    total_data = 0
+    total_rtt = 0    
     while True:
         try:
             # Create a TCP/IP socket
@@ -28,19 +32,24 @@ def measure_rtt(message, timeout=2):
 
             # Receive response
             data = sock.recv(1024)
+            total_data += 1024
+                
             end_time = time.time()
 
             # Calculate RTT
             rtt = end_time - start_time
             rtt *= 1000
-            print(f"Received: {data.decode()}")
-            print(f"Round Trip Time (RTT): {rtt:.0f} ms")
+            total_rtt += rtt
+            if total_data > DATA_SIZE:
+                print(f"Received: {data.decode()}")
+                print(f"Round Trip Time (RTT): {total_rtt:.0f} ms")
+                total_data = 0
+                total_rtt = 0
 
         except socket.timeout:
             print("Request timed out.")
         except Exception as e:
             print(f"An error occurred: {e}")
-        time.sleep(0.1)
 
 if __name__ == "__main__":
     message = "ping"    # Message to send to the server
